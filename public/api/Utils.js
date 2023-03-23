5f766fad8d373eb5f04ea7700f651d7c8267f24b
Luca-Uggetti <128622112+Luca-Uggetti@users.noreply.github.com>
2023-03-23 11:17:08 +0100

    Delete Utils.js

--- public/api/Utils.js
+++
@@ -1,7 +0,0 @@
-export async function getWeather(cityName) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9d2e49fe85d1b1b43bb2038bf1c4344b
-      `
-  ).then((res) => res.json());
-  return response;
-}
